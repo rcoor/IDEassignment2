42de2df4431a05943e4124abca669a7557df08ec
Theis <thornjakobsen@gmail.com>
2016-12-06 20:46:11 +0100

    new branch

--- script.js
+++ script.js
@@ -1,41 +1,10 @@
 /** On page load */
 d3.select(window).on('load', init);
 
-
-function select() {
-    return d3.select('div.box')
-        .selectAll('div.box-element')
-        .style('background-color', 'tomato')
-        .text('Hello');
-}
-
-
 function init() {
-
-    var hhdata = [
-        { 'name': 'Arthur Dent', 'age': 42 },
-        { 'name': 'Ford Pence', 'age': 22 },
-        { 'name': 'Zaphod Beetlebox', 'age': 100 }
-    ]
-
-    d3.selectAll('div.box')
-        .append('box-child')
-        .selectAll('div')
-        .data(hhdata)
-        .enter()
-        .append('div')
-        .text(function (d, i) {
-            return d.name;
-        })
-        .style('background-color', function (d) {
-            if (d.age > 50) return 'tomato';
-            if (d.age <= 50) return 'green';
-        });
-    //select();
     temperatureChange();
+    temperatureChange2();
     barChartInit();
-
-
 }
 
 function temperatureChange() {
@@ -62,9 +31,11 @@ function temperatureChange() {
         return d;
     },
         function (error, data) {
-            data.pop();
-            data.pop();
-            data.pop();
+            var first50 = data.slice(0, 50);
+            var last = data.slice(0, data.length - 4).slice(50, -1);
+            var meanFirst50 = meanOfElementInObjects(first50);
+
+            data = data.slice(0, data.length - 4);
 
 
             if (error) throw error;
@@ -135,6 +106,123 @@ function temperatureChange() {
                 });
             }
         });
+}
+
+function temperatureChange2() {
+    // set the dimensions and margins of the graph
+
+    var svg = d3.select(".multiLine2 svg"),
+        margin = { top: 20, right: 50, bottom: 100, left: 50 },
+        width = +svg.attr("width") - margin.left - margin.right,
+        height = +svg.attr("height") - margin.top - margin.bottom,
+        g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var x = d3.scaleLinear()
+        .range([0, width]);
+
+    var y = d3.scaleLinear()
+        .rangeRound([height, 0]);
+
+    var line = d3.line()
+        .x(function (d) { return x(d.month); })
+        .y(function (d) { return y(d.temp); });
+
+    /** Get data */
+    d3.tsv("data.tsv", function (d) {
+        return d;
+    },
+        function (error, data) {
+            var first50 = data.slice(0, 50);
+            var last = data.slice(0, data.length - 4).slice(50, -1);
+            var meanFirst50 = meanOfElementInObjects(first50);
+
+            data = data.slice(0, data.length - 4);
+
+
+            if (error) throw error;
+
+            // Set domain of x and y
+            x.domain(d3.extent(new Array(12), function (d, i) { return i; }));
+            y.domain(d3.extent([-6, 6], function (d) { return d; }));
+
+            var monthNames = ["January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December"
+            ];
+
+            // Append the x-axis
+            g.append("g")
+                .attr("class", "axis axis--x")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(x).tickFormat((i) => monthNames[i])) // lambda function
+                .selectAll("text")
+                .attr("y", 0)
+                .attr("x", 10)
+                .attr("dy", ".35em")
+                .attr("transform", "rotate(45)")
+                .style("text-anchor", "start");
+
+            // Append the y-axis
+            g.append("g")
+                .attr("class", "axis axis--y")
+                .call(d3.axisLeft(y))
+                .append("text")
+                .attr("fill", "#000")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", "0.71em")
+                .style("text-anchor", "end")
+                .text("Temperature");
+
+            // Create color scale scheme
+            var color = d3.scaleLinear().domain([1, last.length])
+                .interpolate(d3.interpolateHcl)
+                .range([d3.rgb("#dbe2dc"), d3.rgb('#294c5f')]);
+
+            // Plot each line by using callback filtered data. Each callback represents one line.
+            createLines(last, function (newData, i) {
+                g.append("path")
+                    .datum(newData)
+                    .attr("class", "line")
+                    .attr("d", line)
+                    .style("stroke", color(i));
+            });
+
+            // Function for looping through object in the data and emit
+            // each year as an array of objects with month and temperature.
+            function createLines(data, callback) {
+                var i = 0;
+                data.forEach(function (year) {
+                    var newData = [];
+
+                    months = Object.keys(year).splice(1, 12);
+                    months.forEach(function (month, i) {
+                        newData.push({
+                            "month": i,
+                            "temp": parseFloat(year[month]) - meanFirst50[month]
+                        });
+                    });
+
+                    callback(newData, i);
+                    i += 1;
+                });
+            }
+        });
+}
+
+function meanOfElementInObjects(inArray) {
+    var sum = 0;
+    var element = inArray.slice(0, 1)[0];
+    months = Object.keys(element).splice(1, 12);
+    inArray.forEach((object, i) => {
+        element.YEAR = 0;
+        months.forEach((month) => {
+            if (i === 0) {
+                element[month] = 0;
+            };
+            element[month] += object[month] / inArray.length;
+        });
+    });
+    return element;
 }
 
 // Barchart
